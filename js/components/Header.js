d9b9bdc7ccc292862d938d6f9f7713a69982b544
toiphamofficial <toiphamofficial@gmail.com>
2025-12-21 10:37:12 +0700

    CSS restructure

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,4 +1,4 @@
-import {users} from '../data.js';
+import { users } from '../data.js';
 
 export default function Header() {
     return `
@@ -7,13 +7,40 @@ export default function Header() {
                     <div class="tab-item">PODCAST</div>
                     <div class="tab-item">LIVE</div>
                 </div>
-
-                <div class="search-bar">
-                    <span class="material-icons-round">
-                        search
-                    </span>
-                    <input type="text" placeholder="Tìm kiếm bài hát">
-                </div>
+                <form class="search-bar-container">
+                    <div class="search-bar">
+                        <span class="material-icons-round">
+                            search
+                        </span>
+                        <input type="text" placeholder="Tìm kiếm bài hát">
+                    </div>
+                    <ul class="search-bar-suggestions">
+                        <li class="suggestion-item">
+                            <img src="/data/songs/thumb/Faded - Alan Walker.png"
+                                alt="Faded - Alan Walker">
+                            <div class="suggestion-info">
+                                <span class="song-title">Faded</span>
+                                <span class="song-artist">Alan Walker</span>
+                            </div>
+                        </li>
+                        <li class="suggestion-item">
+                            <img src="/data/songs/thumb/Đừng Làm Trái Tim Anh Đau - Sơn Tùng MTP.png"
+                                alt="Đừng Làm Trái Tim Anh Đau - Sơn Tùng MTP">
+                            <div class="suggestion-info">
+                                <span class="song-title">Đừng Làm Trái Tim Anh Đau</span>
+                                <span class="song-artist">Sơn Tùng MTP</span>
+                            </div>
+                        </li>
+                        <li class="suggestion-item">
+                            <img src="/data/songs/thumb/Kamin - Emin ft. JONY.png"
+                                alt="Kamin - Emin ft. JONY">
+                            <div class="suggestion-info">
+                                <span class="song-title">Kamin</span>
+                                <span class="song-artist">Emin ft. JONY</span>
+                            </div>
+                        </li>
+                    </ul>
+                </form>
 
                 <div class="user-actions">
                     <button class="btn btn-notification">
